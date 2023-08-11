8915ac5dbaf26520c0d167876a0cb2ae1455440e
BirukMengistu <sheger.shelf3@gmail>
2023-08-11 14:08:48 +0200

    show-cites-based-on-country

---
+++ src/components/select-city-based-country/countries.js
@@ -0,0 +1,29 @@
+export const Counries=[
+    {
+        name:'Ethiopia',
+        city:[
+            "Addis Ababa",
+            "Gonger",
+            "Mekele",
+            "Jemima"
+        ]
+    },
+    {
+        name:'Sweden',
+        city:[
+            "Stockholm",
+            "Uppsala",
+            "Örbro",
+            "Västras"
+        ]
+    },
+    {
+        name:'Norway',
+        city:[
+            "Oslo",
+            "Sala",
+            "okösbrg",
+            "Jemima"
+        ]
+    }
+] 
